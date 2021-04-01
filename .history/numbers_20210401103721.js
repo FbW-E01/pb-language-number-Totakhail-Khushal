1e2ec5154ddf252625b09e94d9bf38bec3bcd654
khushal <totakhailkhushal@gmail.com>
2021-04-01 11:05:34 +0200

    done

---
+++ .history/numbers_20210401103721.js
@@ -0,0 +1,32 @@
+// 1. Concatenation. 
+//  Create two variables.One variable should contain a string value and the other should contain a number.Concatenate the string and the number.
+const string = "Hello world";
+console.log(string);
+
+const number = 123;
+console.log(number);
+
+console.log(string + number);
+
+
+// ```javascript
+// let introSentence = "hello, my name is Fran and I am";
+// let age = 25;
+// ```
+
+// ** Expected Output **:
+//```javascript
+// "hello, my name is Fran and I am 25"
+
+// * In this case, what is the`+` operator doing ? Comment your answer in the js file.
+
+// #### 2. Converting
+// * Create a variable with the value of "1005".Convert it to a number.
+const num = "1005";
+console.log(parseInt(num));
+
+
+// * Create a variable with the value of "10.05".Convert it to a number.
+const num2 = "10.05";
+parseFloat("num2");
+console.log(parseFloat(num2));
